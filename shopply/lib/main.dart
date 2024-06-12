da1a7d6b9f6e413093fe414cc17fb6d6406fcf1b
Salem Akowe <salemakowe@gmail.com>
2024-06-12 21:03:24 +0100

    treated the security threats

--- shopply/lib/main.dart
+++ shopply/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import "package:firebase_core/firebase_core.dart";
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shopply/screens/login.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
